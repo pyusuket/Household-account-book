b815ae0563a599e7bfb4d26e68544888e707e430
pyusuket <shimomura8939@gmail.com>
2022-11-07 00:16:10 +0900

    グラフの追加/editの修正/コントローラの修正

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,12 +12,28 @@ class DashboardController extends Controller
 {
     public function index($year=null, $month=null)
     {
-        $date = Carbon::now()->format('Y-m');
-        $start = $date.'-01 00:00:00';
-        $end = $date.'-31 23:59:59';
-        $posts = Post::where('created_at', '>', $start)->where('created_at', '<', $end)
+        if(is_null($year) || is_null($month)) {
+            $now = Carbon::now();
+        } else {
+            $now = Carbon::create($year, $month);
+        };
+
+        $fixedCostList = [
+            '住居費' ,
+            '水道・光熱費' ,
+            '通信費' ,
+            '税' ,
+            '社会保障費' ,
+            '保険' ,
+            'サブスク' ,
+            'その他固定費' ,
+        ];
+
+        // グラフ①
+        $posts = Post::whereYear('day', '=', $now->year)->whereMonth('day', '=', $now->month)
                         ->where('selection', '支出')
                         ->groupBy('main')
+                        ->whereIn('main', $fixedCostList)
                         ->select(['main', DB::raw("sum(amount) as total")])
                         ->get();
 
@@ -25,7 +41,46 @@ class DashboardController extends Controller
         foreach ($posts->toArray() as $key => $value) {
                 $items[] = [$value['main'], intval($value['total'])];
             }
-        $items = collect($items); //コレクションオブジェクトに変換して文字に描ける
-        return view('dashboard', compact('items'));
+        $items = collect($items);
+
+        // グラフ②
+        $posts = Post::whereYear('day', '=', $now->year)->whereMonth('day', '=', $now->month)
+                        ->where('selection', '支出')
+                        ->groupBy('main')
+                        ->whereNotIn('main', $fixedCostList)
+                        ->select(['main', DB::raw("sum(amount) as total")])
+                        ->get();
+
+        $items1 = [['大項目', '金額']];
+        foreach ($posts->toArray() as $key => $value) {
+            $items1[] = [$value['main'], intval($value['total'])];
+        }
+        $items1 = collect($items1);
+
+        // グラフ③
+        $posts = Post::whereYear('day', '=', $now->year)->whereMonth('day', '=', $now->month)
+                        ->where('selection', '収入')
+                        ->groupBy('main')
+                        ->select(['main', DB::raw("sum(amount) as total")])
+                        ->get();
+
+        $items2 = [['大項目', '金額']];
+        foreach ($posts->toArray() as $key => $value) {
+            $items2[] = [$value['main'], intval($value['total'])];
+        }
+        $items2 = collect($items2);
+
+        // 前月参照ボタン
+        $prev = Carbon::create($now->year, $now->month-1);
+        $prev = ['year' => $prev->year ,'month' => $prev->month];
+
+        // 翌月参照ボタン
+        $next = Carbon::create($now->year, $now->month+1);
+        $next = ['year' => $next->year, 'month' => $next->month];
+
+        // 現在の年と月を取得
+        $displayDate = "{$now->year}年{$now->month}月";
+
+        return view('dashboard', compact('items','items1', 'items2' ,'prev','next', 'displayDate'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,10 +5,19 @@
         </h2>
     </x-slot>
 
-    <div class="py-12 bg-gray-500">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-12 bg-gray-500 ">
+        <div class="flex justify-between w-7/12 mx-auto">
+            <a href="{{ route("dashboard" , $prev) }}"><<</a>
+            <div class=" text-center text-gray-200">{{$displayDate}}</div>
+            <a href="{{ route("dashboard" , $next) }}">>></a>
+        </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div id="donutchart" style="width: 900px; height: 500px;"></div>
+                <div id="donutchart" style="width: 500px; height: 500px;"></div>
+                <div id="donutchart1" style="width: 500px; height: 500px;"></div>
+            </div>
+            <div>
+                <div id="donutchart2" style="width: 500px; height: 500px;"></div>
             </div>
         </div>
     </div>
@@ -21,12 +30,33 @@
         var data = google.visualization.arrayToDataTable({!! $items !!});
 
         var options = {
-          title: '月の収支',
+          title: '変動費',
           pieHole: 0.4,
         };
 
         var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
         chart.draw(data, options);
+
+        var data1 = google.visualization.arrayToDataTable({!! $items1 !!});
+
+        var options1 = {
+          title: '固定費',
+          pieHole: 0.4,
+        };
+
+        var chart1 = new google.visualization.PieChart(document.getElementById('donutchart1'));
+        chart1.draw(data1, options1);
+
+        var data2 = google.visualization.arrayToDataTable({!! $items2 !!});
+
+        var options2 = {
+          title: '収入',
+          pieHole: 0.4,
+        };
+
+        var chart2 = new google.visualization.PieChart(document.getElementById('donutchart2'));
+        chart2.draw(data2, options2);
+
       }
     </script>
 
